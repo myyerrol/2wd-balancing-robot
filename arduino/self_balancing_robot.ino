3acce10d0d0cb7dd5255b3bffa9cf219946cd002
myyerrol <myyerrol@126.com>
2018-02-22 21:09:46 +0800

    Modify the file

--- arduino/self_balancing_robot.ino
+++ arduino/self_balancing_robot.ino
@@ -24,7 +24,8 @@
 #define PIN_MOTOR_ENB        10
 #define MOTOR_A              0
 #define MOTOR_B              1
-#define TIMEOUT              30000
+#define TIMEOUT_SONIC        30000
+#define TIMEOUT_IIC          1000
 
 enum MotorState
 {
@@ -44,11 +45,25 @@ enum RobotState
 
 // Adafruit_SSD1306 g_oled(-1);
 
+// IMU Variables.
+const uint8_t IMU_ADDRESS = 0x70;
+double g_acc_x, g_acc_y, g_acc_z;
+double g_gyro_y, g_gyro_z;
+double g_comp_y_angle, g_gyro_y_angle;
+int16_t g_temp;
+uint32_t g_imu_timer;
+static uint8_t g_iic_buffer[8];
+
 void initMotors(void);
+void initIMU(void);
 void initOLED(void);
 void setMotorDirection(uint8_t motor, MotorState state);
 void setMotorSpeed(uint8_t motor, uint8_t speed);
 void playBuzzerSound(uint8_t freq, uint8_t time);
+uint8_t readDataFromIIC(uint8_t address, uint8_t *data, uint8_t bytes);
+uint8_t writeDataToIIC(uint8_t address, uint8_t data, bool stop_flag);
+uint8_t writeDataToIIC(uint8_t address, uint8_t *data, uint8_t length,
+                       bool stop_flag);
 float getUltrasonicDistance(void);
 
 void initMotors(void)
@@ -62,6 +77,40 @@ void initMotors(void)
 
     setMotorDirection(MOTOR_A, MOTOR_STOP);
     setMotorDirection(MOTOR_B, MOTOR_STOP);
+}
+
+void initIMU(void)
+{
+    g_iic_buffer[0] = 7;
+    g_iic_buffer[1] = 0x00;
+    g_iic_buffer[2] = 0x00; // Set Gyroscope Full Scale Range to ±250deg/s.
+    g_iic_buffer[3] = 0x00; // Set Accelerometer Full Scale Range to ±2g.
+
+    // Write to all four registers at once.
+    while (writeDataToIIC(0x19, g_iic_buffer, 4, false));
+    // PLL with X axis gyroscope reference.
+    while (writeDataToIIC(0x6B, 0x01, true));
+    while (readDataFromIIC(0x75, g_iic_buffer, 1));
+
+    if (g_iic_buffer[0] != 0x70) {
+        while (1);
+    }
+
+    delay(1000);
+
+    while (readDataFromIIC(0x3B, g_iic_buffer, 6));
+    g_acc_x = (g_iic_buffer[0] << 8) | g_iic_buffer[1];
+    g_acc_y = (g_iic_buffer[2] << 8) | g_iic_buffer[3];
+    g_acc_z = (g_iic_buffer[4] << 8) | g_iic_buffer[5];
+    g_temp  = (g_iic_buffer[6] << 8) | g_iic_buffer[7];
+
+    double pitch = atan(-g_acc_x / sqrt(g_acc_y * g_acc_y + g_acc_z * g_acc_z))
+        * RAD_TO_DEG;
+
+    g_gyro_y_angle = pitch;
+    g_comp_y_angle = pitch;
+
+    g_imu_timer = micros();
 }
 
 // void initOLED(void)
@@ -144,6 +193,66 @@ void playBuzzerSound(uint8_t freq, uint8_t time)
     noTone(PIN_BUZZER);
 }
 
+uint8_t writeDataToIIC(uint8_t address, uint8_t data, bool stop_flag)
+{
+    return writeDataToIIC(address, &data, 1, stop_flag);
+}
+
+uint8_t writeDataToIIC(uint8_t address, uint8_t *data, uint8_t length,
+                       bool stop_flag)
+{
+    Wire.beginTransmission(IMU_ADDRESS);
+    Wire.write(address);
+    Wire.write(data, length);
+
+    uint8_t result = Wire.endTransmission(stop_flag);
+
+    if (result) {
+        Serial.print(F("writeDataToIIC() failed: "));
+        Serial.println(result);
+    }
+
+    return result;
+}
+
+uint8_t readDataFromIIC(uint8_t address, uint8_t *data, uint8_t bytes)
+{
+    uint32_t timeout_timer;
+
+    Wire.beginTransmission(IMU_ADDRESS);
+    Wire.write(address);
+
+    uint8_t result = Wire.endTransmission(false);
+
+    if (result) {
+      Serial.print(F("readDataFromIIC() failed: "));
+      Serial.println(result);
+      return result;
+    }
+
+    Wire.requestFrom(IMU_ADDRESS, bytes, (uint8_t)true);
+
+    for (uint8_t i = 0; i < bytes; i++)
+    {
+        if (Wire.available()) {
+            data[i] = Wire.read();
+        }
+        else {
+            timeout_timer = micros();
+            while (((micros() - timeout_timer) < TIMEOUT_IIC) &&
+                   !Wire.available());
+            if (Wire.available()) {
+                data[i] = Wire.read();
+            }
+            else {
+                Serial.println(F("readDataFromIIC() timeout!"));
+                return 5;
+            }
+        }
+    }
+    return 0;
+}
+
 float getUltrasonicDistance(void)
 {
     float duration = 0.0;
@@ -155,7 +264,7 @@ float getUltrasonicDistance(void)
     delayMicroseconds(10);
     digitalWrite(PIN_TRIG, LOW);
 
-    duration = pulseIn(PIN_ECHO, HIGH, TIMEOUT);
+    duration = pulseIn(PIN_ECHO, HIGH, TIMEOUT_SONIC);
 
     if (duration == 0.0) {
         return 0.0;
@@ -178,6 +287,7 @@ void setup()
     initMotors();
     // initOLED();
 
+    Wire.begin();
     Serial.begin(115200);
 }
 
