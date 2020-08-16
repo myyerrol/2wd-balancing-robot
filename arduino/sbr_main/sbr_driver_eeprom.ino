b7180080dac92af4826a5075abb11c15132b2fdb
myyerrol <myyerrol@126.com>
2020-08-16 22:29:12 +0800

    Finish eeprom's code

--- arduino/sbr_main/sbr_driver_eeprom.ino
+++ arduino/sbr_main/sbr_driver_eeprom.ino
@@ -1,19 +1,16 @@
-void initEEPROM(void)
-{
+void initEEPROM(void) {
     for (uint8_t i = 0; i < 4; i++) {
         g_eeprom.bytes[i] = 0;
     }
 }
 
-void readDataFromEEPROM(void)
-{
+void readDataFromEEPROM(void) {
     for (uint8_t i = 0; i < 4; i++) {
         g_eeprom.bytes[i] = EEPROM.read(i);
     }
 }
 
-void writeDataToEEPROM(void)
-{
+void writeDataToEEPROM(void) {
     uint8_t *ptr = g_eeprom.bytes;
 
     for (uint8_t i = 0; i < 4; i++) {
