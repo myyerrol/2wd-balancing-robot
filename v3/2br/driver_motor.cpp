b88c3fdd8a05949e7278e1b6cb29fb6688c59e27
myyerrol <myyerrol@126.com>
2024-07-23 09:00:00 +0800

    style: rename pin macro

--- v3/2br/driver_motor.cpp
+++ v3/2br/driver_motor.cpp
@@ -1,32 +1,32 @@
 #include "driver_motor.hpp"
 
 void initMotor() {
-    ledcAttach(PIN_MOTOR_L1, LEDC_FREQ_1000HZ, LEDC_RESO_10BITS);
-    ledcAttach(PIN_MOTOR_L2, LEDC_FREQ_1000HZ, LEDC_RESO_10BITS);
-    ledcAttach(PIN_MOTOR_R1, LEDC_FREQ_1000HZ, LEDC_RESO_10BITS);
-    ledcAttach(PIN_MOTOR_R2, LEDC_FREQ_1000HZ, LEDC_RESO_10BITS);
+    ledcAttach(MOTOR_PIN_L1, MOTOR_FREQ_1000HZ, LEDC_RESO_10BITS);
+    ledcAttach(MOTOR_PIN_L2, MOTOR_FREQ_1000HZ, LEDC_RESO_10BITS);
+    ledcAttach(MOTOR_PIN_R1, MOTOR_FREQ_1000HZ, LEDC_RESO_10BITS);
+    ledcAttach(MOTOR_PIN_R2, MOTOR_FREQ_1000HZ, LEDC_RESO_10BITS);
 }
 
 void setMotorDirAndSpeed(int motor, float pwm) {
     pwm = (int)pwm;
     if (motor == MOTOR_L) {
         if (pwm < 0) {
-            ledcWrite(PIN_MOTOR_L1, 0);
-            ledcWrite(PIN_MOTOR_L2, -pwm);
+            ledcWrite(MOTOR_PIN_L1, 0);
+            ledcWrite(MOTOR_PIN_L2, -pwm);
         }
         else {
-            ledcWrite(PIN_MOTOR_L1, pwm);
-            ledcWrite(PIN_MOTOR_L2, 0);
+            ledcWrite(MOTOR_PIN_L1, pwm);
+            ledcWrite(MOTOR_PIN_L2, 0);
         }
     }
     else if (motor == MOTOR_R) {
         if (pwm < 0) {
-            ledcWrite(PIN_MOTOR_R1, -pwm);
-            ledcWrite(PIN_MOTOR_R2, 0);
+            ledcWrite(MOTOR_PIN_R1, -pwm);
+            ledcWrite(MOTOR_PIN_R2, 0);
         }
         else {
-            ledcWrite(PIN_MOTOR_R1, 0);
-            ledcWrite(PIN_MOTOR_R2, pwm);
+            ledcWrite(MOTOR_PIN_R1, 0);
+            ledcWrite(MOTOR_PIN_R2, pwm);
         }
     }
     else {
--- v3/2br/driver_motor.hpp
+++ v3/2br/driver_motor.hpp
@@ -4,12 +4,12 @@
 #include <Arduino.h>
 #include "2br.hpp"
 
-#define PIN_MOTOR_L1 0
-#define PIN_MOTOR_L2 1
-#define PIN_MOTOR_R1 2
-#define PIN_MOTOR_R2 3
+#define MOTOR_PIN_L1 0
+#define MOTOR_PIN_L2 1
+#define MOTOR_PIN_R1 2
+#define MOTOR_PIN_R2 3
 
-#define LEDC_FREQ_1000HZ 1000
+#define MOTOR_FREQ_1000HZ 1000
 #define LEDC_RESO_10BITS 10
 
 #define MOTOR_L 0
