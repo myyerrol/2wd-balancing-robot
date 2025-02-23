4d322c5b1652269f184156544c539de6a6b8775f
myyerrol <myyerrol@126.com>
2025-02-23 09:00:00 +0800

    feat: add motor src

--- v3/2br/2br.ino
+++ v3/2br/2br.ino
@@ -1,9 +1,21 @@
-
-
 void setup() {
-
+    Serial.begin(9600);
+    pinMode(0, OUTPUT);
+    pinMode(1, OUTPUT);
 }
 
 void loop() {
-
+    Serial.println("Hello World!");
+    delay(1000);
+    digitalWrite(0, LOW);
+    digitalWrite(1, LOW);
+    delay(1000);
+    digitalWrite(0, LOW);
+    digitalWrite(1, HIGH);
+    delay(1000);
+    digitalWrite(0, HIGH);
+    digitalWrite(1, LOW);
+    delay(1000);
+    digitalWrite(0, HIGH);
+    digitalWrite(1, HIGH);
 }
---
+++ v3/2br/driver_motor.hpp
@@ -0,0 +1,9 @@
+#ifndef DRIVER_MOTOR
+#define DRIVER_MOTOR
+
+#define PIN_MOTOR_L1 0
+#define PIN_MOTOR_L2 1
+#define PIN_MOTOR_R1 2
+#define PIN_MOTOR_R2 3
+
+#endif
