ce93db3ca922887b3e9fb9aff56d85221d909560
myyerrol <myyerrol@126.com>
2025-02-27 09:00:00 +0800

    style: convert zh comment to en

--- v1/arduino/2br_main/2br_driver_oled.ino
+++ v1/arduino/2br_main/2br_driver_oled.ino
@@ -2,11 +2,11 @@
 void initOLED(void) {
     g_oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
     g_oled.clearDisplay();
-    // g_oled.setTextSize(2);
-    // g_oled.setTextColor(WHITE);
-    // g_oled.setCursor(0, 0);
-    // g_oled.print("SBR");
-    // g_oled.display();
+    g_oled.setTextSize(2);
+    g_oled.setTextColor(WHITE);
+    g_oled.setCursor(0, 0);
+    g_oled.print("SBR");
+    g_oled.display();
 
     g_oled.drawBitmap((g_oled.width()  - OLED_LOGO_WIDTH ) / 2,
                       (g_oled.height() - OLED_LOGO_HEIGHT) / 2,
@@ -21,16 +21,18 @@ void showSelfCheckingInfo(void) {
     g_oled.setTextSize(1);
     g_oled.setCursor(0, 0);
 
-    // 将Flash程序存储空间中的常量读取到SRAM动态内存中，减少SRAM运行时的资源占用
-   for (uint8_t i = 0; i < strlen(g_info_check); i++) {
-       // 采用16位短地址寻址模式，最多可以寻址64kB的数据
-       uint8_t ch = pgm_read_byte_near(g_info_check + i);
-       g_oled.print(ch);
-   }
-   for (uint8_t i = 0; i < strlen(g_info_temp); i++) {
-       uint8_t ch = pgm_read_byte_near(g_info_temp + i);
-       g_oled.print(ch);
-   }
+    // Read constants from Flash program storage space to SRAM dynamic memory
+    // to reduce resource consumption during SRAM runtime.
+    for (uint8_t i = 0; i < strlen(g_info_check); i++) {
+        // Adopting a 16 bit short address addressing mode, it can address up
+        // to 64KB data.
+        uint8_t ch = pgm_read_byte_near(g_info_check + i);
+        g_oled.print(ch);
+    }
+    for (uint8_t i = 0; i < strlen(g_info_temp); i++) {
+        uint8_t ch = pgm_read_byte_near(g_info_temp + i);
+        g_oled.print(ch);
+    }
 
     g_oled.print((double)g_temp / 340.0 + 31.53);
     g_oled.display();
