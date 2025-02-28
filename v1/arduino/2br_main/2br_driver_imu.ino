50a253ccd392665738321341090528bed0f0c793
myyerrol <myyerrol@126.com>
2025-02-28 09:00:00 +0800

    style: convert zh comment to en

--- v1/arduino/2br_main/2br_driver_imu.ino
+++ v1/arduino/2br_main/2br_driver_imu.ino
@@ -1,54 +1,57 @@
 void initIMU(void) {
-    // 读取【WHO_AM_I】寄存器数据
+    // Read data from "WHO_AM_I" register.
     while (readDataFromIIC(REG_WHO_AM_I, g_iic_buffer, 1));
     if (g_iic_buffer[0] != 0x70) {
         while (1);
     }
 
-    // 重设内部寄存器组并恢复默认设置
+    // Reset internal registers and restore default settings.
     while (writeDataToIIC(REG_PWR_MGMT_1, 0x80, true));
     delay(100);
     while (writeDataToIIC(REG_SIGNAL_PATH_RESET, 0x07, true));
     delay(100);
 
-    // 如果PLL准备就绪则自动选择最可靠的时钟源，否则使用内部晶振
+    // If PLL is ready, select the most reliable clock source automatically,
+    // otherwise use an internal crystal oscillator.
     while (writeDataToIIC(REG_PWR_MGMT_1, 0x01, true));
-    // 使能加速度计和陀螺仪
+    // Enable accelerometer and gyroscope.
     while (writeDataToIIC(REG_PWR_MGMT_2, 0x00, true));
 
-    // 内部寄存器为：REG_SMPLRT_DIV
-    // 设置采样频率为1000Hz: 1kHz/(1+0) = 1000Hz
+    // The internal register: REG_SMPLRT_DIV.
+    // Set the sampling frequency to 1000Hz: 1KHz / (1 + 0) = 1000Hz.
     g_iic_buffer[0] = 0x00;
-    // 内部寄存器为：REG_CONFIG
-    // 设置FIFO模式为0、禁用FSYNC并设置陀螺仪滤波模式（184Hz带宽、2.9ms延迟、1kHz帧同步）
+    // The internal register: REG_CONFIG.
+    // Set FIFO mode to 0, disable FSYNC and set gyroscope filter mode (184Hz
+    // bandwidth, 2.9ms latency, 1KHz frame synchronization).
     g_iic_buffer[1] = 0x01;
-    // 内部寄存器为：REG_GYRO_CONFIG
-    // 设置陀螺仪满标度范围为±250deg/s
+    // The internal register: REG_GYRO_CONFIG.
+    // Set the gyroscope full scale range to ±250deg/s.
     g_iic_buffer[2] = 0x00;
-    // 内部寄存器为：REG_ACCEL_CONFIG
-    // 设置加速度计满标度范围为±2g
+    // The internal register: REG_ACCEL_CONFIG.
+    // Set the accelerometer full scale range to ±2g.
     g_iic_buffer[3] = 0x00;
-    // 内部寄存器为：REG_ACCEL_CONFIG_2
-    // 设置加速度计滤波模式（460Hz带宽、1.94ms延迟、1kHz帧同步）
+    // The internal register: REG_ACCEL_CONFIG_2.
+    // Set the accelerometer filtering mode (460Hz bandwidth, 1.94ms latency,
+    // 1KHz frame synchronization).
     g_iic_buffer[4] = 0x00;
-    // 将配置信息写入到寄存器组中
+    // Write configuration information to the registers.
     while (writeDataToIIC(REG_SMPLRT_DIV, g_iic_buffer, 5, true));
 
-    // 等待传感器初始化完成
+    // Wait for the sensor initialization to complete.
     delay(100);
 
-    // 获取传感器的加速度和温度数值
+    // Read the acceleration and temperature values of the sensor.
     while (readDataFromIIC(0x3B, g_iic_buffer, 8));
     g_acc_x = (g_iic_buffer[0] << 8) | g_iic_buffer[1];
     g_acc_y = (g_iic_buffer[2] << 8) | g_iic_buffer[3];
     g_acc_z = (g_iic_buffer[4] << 8) | g_iic_buffer[5];
     g_temp  = (g_iic_buffer[6] << 8) | g_iic_buffer[7];
 
-    // 计算得到初始俯仰角
+    // Calculate the initial pitch angle.
     double pitch = atan(-g_acc_x / sqrt(g_acc_y * g_acc_y + g_acc_z * g_acc_z))
         * RAD_TO_DEG;
 
-    // 计算得到绕Y轴旋转角
+    // Calculate the rotation angle around the Y-axis.
     g_gyro_y_angle = pitch;
     g_calc_y_angle = pitch;
 
@@ -60,7 +63,7 @@ void filterIMUData(void) {
     delay(10);
 #endif
 
-    // 获取传感器的加速度、温度和陀螺仪数值
+    // Read acceleration, temperature, and gyroscope values from sensors.
     while (readDataFromIIC(0x3B, g_iic_buffer, 14));
     g_acc_x  = (g_iic_buffer[0]  << 8) | g_iic_buffer[1];
     g_acc_y  = (g_iic_buffer[2]  << 8) | g_iic_buffer[3];
@@ -74,14 +77,14 @@ void filterIMUData(void) {
     double dt = (double)(micros() - g_timer_imu) / 1000000;
     g_timer_imu = micros();
 
-    // 计算得到当前俯仰角
+    // Calculate the initial pitch angle.
     double pitch = atan(-g_acc_x / sqrt(g_acc_y * g_acc_y + g_acc_z * g_acc_z))
         * RAD_TO_DEG;
     double gyro_y_rate = g_gyro_y / 131.0;
 
     calculateAnglePID();
 
-    // 计算得到绕Y轴旋转角
+    // Calculate the rotation angle around the Y-axis.
     g_gyro_y_angle += gyro_y_rate * dt;
     g_calc_y_angle  = 0.93 * (g_calc_y_angle + gyro_y_rate * dt) + 0.07 * pitch;
 
